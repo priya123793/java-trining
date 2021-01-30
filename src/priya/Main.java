47a362e7e11fd6ffb6f37648019a717263a30633
priya123793 <priya.mekala96@gmail.com>
2021-01-30 17:51:53 +0530

    initial commit

---
+++ src/priya/Main.java
@@ -0,0 +1,12 @@
+package priya;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner s=new Scanner(System.in);
+    private static int[] ("Enter 10 integers:");
+
+    }
+}
